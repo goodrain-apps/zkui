506ac23a17bd0b2382affcb5568071c7548c8e0e
zhouyq <zyq916@gmail.com>
2016-06-06 18:13:04 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+[ $DEBUG ] && set -x
+
+sed -i -r "s/(zkServer)=.*/\1=${ZK_HOST}:${ZK_PORT}/"  $ZKUI_CFG
+sed -i -r "#(\"password\"):\"manager\"#\1:\"${ZKUI_PASS}\"#" $ZKUI_CFG
+
+sleep ${PAUSE:-0}
+
+cd $ZKUI_DIR
+java -jar zkui-2.0-SNAPSHOT-jar-with-dependencies.jar
